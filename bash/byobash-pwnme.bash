7fb54c13194eb50db79093a1185da0ece1bd7c41
Pat LaVarre <plavarre@purestorage.com>
2022-07-06 16:55:03 -0700

    pwnme:  Help people drop 'byobash/' in places other than:  ~/Public/byobash/

--- bash/byobash-pwnme.bash
+++ bash/byobash-pwnme.bash
@@ -1,5 +1,11 @@
+# usage: qbin/pwnme
+# usage: bash/byobash-pwnme.bash
+
 set -xeuo pipefail
-cd ~/Public/byobash
+
+cd "$(dirname $0)"/..
+dirs -p |tail -1
+
 git fetch
 git rebase
 git log --oneline --no-decorate -1
